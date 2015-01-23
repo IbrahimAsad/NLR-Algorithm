9a0b4c96fb724a68e14d326dd8d9f1f733a02a18
Ibrahim Asad <ibra.asad@gmail.com>
2015-01-23 10:18:25 +0200

    Add main classes
    add classes

---
+++ src/algorithm/ui/MainView.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package algorithm.ui;
+
+/**
+ * @author Ibrahim
+ *
+ */
+public class MainView {
+
+	/**
+	 * 
+	 */
+	public MainView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
