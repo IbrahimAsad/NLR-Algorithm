bcbe00cb10ddce691f9593678951376ab9aedd70
Ibrahim Asad <ibra.asad@gmail.com>
2015-02-02 23:42:59 +0200

    the result now the same in the excel file

--- src/algorithm/core/Extractor.java
+++ src/algorithm/core/Extractor.java
@@ -3,6 +3,7 @@
  */
 package algorithm.core;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 /**
@@ -35,14 +36,14 @@ public class Extractor {
 		return Math.cos(x);
 	}
 	
-	private double calculateBi(){
-		System.out.println(ti);
-   		return atan((sin(Math.toRadians(B0))) +(P*ti),(cos(Math.toRadians(B0))+(Q*ti)));
+	private double calculateBi(){ 
+		DecimalFormat x=new DecimalFormat("###.############");
+		return atan((sin(Math.toRadians(B0))) +Double.parseDouble(x.format(P*ti)),(cos(Math.toRadians(B0))+Double.parseDouble(x.format(Q*ti))));
 
 		//		//=DEGREES(ATAN2(SIN(RADIANS(-54))+(7*0.07175925934),COS(RADIANS(-54))+(-27*0.07175925934))
 				//=DEGREES(ATAN2(SIN(RADIANS($L$2))+$L$3*C75,COS(RADIANS($L$2))+$L$4*C75))
  	}
-	
+
 	/**
 	 * 
 	 */
@@ -55,22 +56,25 @@ public class Extractor {
 	}
 	
 	public void evaluateDataSet(){
-		for(int i=1;i<data.size();i++){
+		for(int i=2;i<data.size();i++){
 			double orgB=data.get(i).getBearingDegs();
 			double curB=eval();
 			orgBearing.add(orgB);
 			currentBearing.add(curB);
-			double curError=Math.pow(orgB-curB, 2);
+			double curError=Math.pow((curB-orgB), 2);
 			totalError+=curError;
 			error_List.add(curError);
 			incrementTime();
 			
 		}
-		for(int i=1;i<orgBearing.size();i++){
+		for(int i=0;i<orgBearing.size();i++){
 //			System.out.println(orgBearing.get(i) +" - "+ currentBearing.get(i) +"= "+error_List.get(i));
-			System.out.println(currentBearing.get(i));
+//			System.out.println(currentBearing.get(i));
 		}
 		System.out.println("total erros "+totalError);
+		System.out.println("P="+P);
+		System.out.println("Q="+Q);
+		System.out.println("B0="+B0);
 	}
 	
 	public double eval(){
